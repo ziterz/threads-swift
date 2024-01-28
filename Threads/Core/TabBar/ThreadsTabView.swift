5d7f6ea88fc6ff8cbd182bfc30596e9478a0d0c6
Ziterz <ziady.mubaraq@gmail.com>
2024-01-28 23:02:24 +0700

    feat: completing Create New Thread sheet

--- Threads/Core/TabBar/ThreadsTabView.swift
+++ Threads/Core/TabBar/ThreadsTabView.swift
@@ -9,7 +9,8 @@
 import SwiftUI
 
 struct ThreadsTabView: View {
-  @State private var selectedTab = 0
+  @State private var selectedTab: Int = 0
+  @State private var showCreateThreadView: Bool = false
   
   var body: some View {
     TabView(selection: $selectedTab) {
@@ -28,7 +29,7 @@ struct ThreadsTabView: View {
         .onAppear { selectedTab = 1 }
         .tag(1)
       
-      CreateThreadView()
+      Text("")
         .tabItem {
           Image(systemName: "plus")
         }
@@ -51,6 +52,14 @@ struct ThreadsTabView: View {
         .onAppear { selectedTab = 4 }
         .tag(4)
     }
+    .onChange(of: selectedTab, { oldValue, newValue in
+      showCreateThreadView = selectedTab == 2
+    })
+    .sheet(isPresented: $showCreateThreadView, onDismiss: {
+      selectedTab = 0
+    }, content: {
+      CreateThreadView()
+    })
     .tint(.black)
   }
 }
--- Threads/Core/ThreadCreation/View/CreateThreadView.swift
+++ Threads/Core/ThreadCreation/View/CreateThreadView.swift
@@ -9,11 +9,66 @@
 import SwiftUI
 
 struct CreateThreadView: View {
-    var body: some View {
-        Text("Create Thread!")
+  @State private var caption: String = ""
+  @Environment(\.dismiss) var dismiss
+  
+  var body: some View {
+    NavigationStack {
+      VStack {
+        HStack(alignment: .top) {
+          CircularProfileImageView()
+          
+          VStack(alignment: .leading, spacing: 4) {
+            Text("ziterz")
+              .fontWeight(.semibold)
+            
+            TextField("Start a thread...", text: $caption, axis: .vertical)
+          }
+          .font(.footnote)
+          
+          Spacer()
+          
+          if !caption.isEmpty {
+            Button {
+              caption = ""
+            } label: {
+              Image(systemName: "xmark")
+                .resizable()
+                .frame(width: 12, height: 12)
+                .foregroundStyle(Color(.gray))
+            }
+          }
+        }
+        
+        Spacer()
+      }
+      .padding()
+      .navigationTitle("New Thread")
+      .navigationBarTitleDisplayMode(.inline)
+      .toolbar {
+        ToolbarItem(placement: .topBarLeading) {
+          Button("Cancel") {
+            dismiss()
+          }
+          .font(.subheadline)
+          .foregroundStyle(Color(.black))
+        }
+        
+        ToolbarItem(placement: .topBarTrailing) {
+          Button("Post") {
+            
+          }
+          .opacity(caption.isEmpty ? 0.5 : 1.0)
+          .disabled(caption.isEmpty)
+          .font(.subheadline)
+          .fontWeight(.semibold)
+          .foregroundStyle(Color(.black))
+        }
+      }
     }
+  }
 }
 
 #Preview {
-    CreateThreadView()
+  CreateThreadView()
 }
