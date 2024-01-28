55f05eb4a12f9e926ff507a4449c8a0a439c8a12
Ziterz <ziady.mubaraq@gmail.com>
2024-01-28 16:06:15 +0700

    Initial Commit

---
+++ Threads/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Threads
+//
+//  Created by Ziady Mubaraq on 28/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Ziady Mubaraq on 28/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
