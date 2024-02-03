0a03e1e32b6668c81176c6bcfba02a08c1cb08e2
Ziterz <ziady.mubaraq@gmail.com>
2024-02-03 15:52:55 +0700

    feat: create image uploader to firebase storage and get the url string to populate the profile

--- Threads/Core/Profile/EditProfile/View/EditProfileView.swift
+++ Threads/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -14,7 +14,7 @@ struct EditProfileView: View {
   @State private var link: String = ""
   @State private var isPrivateProfile: Bool = false
   @Environment(\.dismiss) var dismiss
-  @EnvironmentObject var viewModel: CurrentUserProfileViewModel
+  @StateObject var viewModel = EditProfileViewModel()
   
   var body: some View {
     NavigationStack {
@@ -91,7 +91,10 @@ struct EditProfileView: View {
         
         ToolbarItem(placement: .topBarTrailing) {
           Button("Done") {
-            
+            Task {
+              try await viewModel.updateUserData()
+              dismiss()
+            }
           }
           .font(.subheadline)
           .fontWeight(.semibold)
